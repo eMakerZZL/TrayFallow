18751d96616322f859502c3a68ad46faf0a996bc
eMakerZZL <89433175@qq.com>
2019-07-23 13:10:44 +0800

    add k > 1 environment

--- Myline.m
+++ Myline.m
@@ -6,7 +6,7 @@ x = 1;
 y = 2;
 
 start_point = [0;0];
-end_point = [10;20];
+end_point = [10;10];
 
 line_start = [start_point(x) end_point(x)];
 line_end = [start_point(y) end_point(y)];
@@ -15,38 +15,47 @@ plot(line_start,line_end);
 delta_x = end_point(x) - start_point(x);
 delta_y = end_point(y) - start_point(y);
 distance = sqrt(delta_x^2 + delta_y^2);
+
 k = delta_y / delta_x;
 
-unit_length = 0.05;
+unit_length = 0.5;
 line_segment = floor(distance / unit_length);
 last_line_length = distance - line_segment * unit_length;
 
-A_point = start_point;
+Insert_Point= start_point;
 Insert_Line = start_point';
 
 for i = 1 : 1 : line_segment
-    B_point = [A_point(x) + unit_length;A_point(y)];
-    C_point = [A_point(x) + unit_length;A_point(y) + unit_length];
+    target_Point(x) = unit_length * i;
+    target_Point(y) = target_Point(x) * k;
 
-    F_real_coord(x) = unit_length * i;
-    F_real_coord(y) = F_real_coord(x) * k;
-    F_delta_X = A_point(y) - F_real_coord(y);
-    F_delta_Y = C_point(y) - F_real_coord(y);
-    res = F_delta_X + F_delta_Y;
-
-    if(res > 0)
-        if(A_point(x) > end_point(x) || A_point(y) > end_point(y)) break
-        end
-        plot(B_point(x),B_point(y),'-o');
-        A_point(x) = A_point(x) + unit_length;
-    else
-        if(C_point(x) > end_point(x) || C_point(y) > end_point(y)) break
-        end
-        plot(C_point(x),C_point(y),'-*');
-        A_point(x) = A_point(x) + unit_length;
-        A_point(y) = A_point(y) + unit_length;
+     if (target_Point(x) > 0 && target_Point(y) > 0)
+         if (k > 0 && k < 1)
+             epsilon_single_axis = Insert_Point(y) - target_Point(y);
+             epsilon_double_axis = Insert_Point(y) - target_Point(y) + unit_length;
+             res = epsilon_single_axis + epsilon_double_axis;
+             if(res > 0)
+                 Insert_Point(x) = Insert_Point(x) + unit_length;
+             else
+                 Insert_Point(x) = Insert_Point(x) + unit_length;
+                 Insert_Point(y) = Insert_Point(y) + unit_length;
+             end
+         else
+             epsilon_single_axis = Insert_Point(x) - target_Point(x);
+             epsilon_double_axis = Insert_Point(x) - target_Point(x) + unit_length;
+             res = epsilon_single_axis + epsilon_double_axis;
+             if(res > 0)
+                 Insert_Point(y) = Insert_Point(y) + k * unit_length;
+             else
+                 Insert_Point(x) = Insert_Point(x) + k * unit_length;
+                 Insert_Point(y) = Insert_Point(y) + k * unit_length;
+             end
+         end
+            if(Insert_Point(x) > end_point(x) || Insert_Point(y) > end_point(y)) break
+            end
     end
 
-    Insert_Line = [Insert_Line;A_point'];
+    Insert_Line = [Insert_Line;Insert_Point'];
 end
+
 plot(Insert_Line(:,1),Insert_Line(:,2),'r');
